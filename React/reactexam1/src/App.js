af3fd1a4378097c5b5ecc144e7bb9df7afb6d9f3
nara04040 <nara04040@naver.com>
2022-09-29 12:58:14 +0900

    [React]State

--- React/reactexam1/src/App.js
+++ React/reactexam1/src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import "./App.css";
+import Counter from "./Counter";
 import MyHeader from "./MyHeader";
-import MyFooter from "./MyFooter";
+
 function App() {
-  let name = "nara";
-  const style = {
-    App: {
-      backgroundColor: "yellow",
-    },
-  };
   const number = 5;
   return (
-    <div className="App" style={style.App}>
+    <div>
       <MyHeader />
-      <h2>안녕 리액트 {1 + 2}</h2>
-      <b id="bold-text">
-        {number} 는 {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
-      <MyFooter />
+      <Counter />
     </div>
   );
 }
