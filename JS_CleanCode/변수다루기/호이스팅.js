18848f1569614741192068db58af95cea3e5e519
nara04040 <nara04040@naver.com>
2022-10-18 12:21:31 +0900

    [js_cleancode] 타입검사

---
+++ JS_CleanCode/변수다루기/호이스팅.js
@@ -0,0 +1,20 @@
+var global = 0;
+
+function outer() {
+  console.log(global); // undefined
+  var global = 5;
+
+  function inner() {
+    var global = 10;
+
+    console.log(global); // 10
+  }
+
+  inner();
+
+  global = 1;
+
+  console.log(global); // 1
+}
+
+outer();
