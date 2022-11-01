3d3177b05709c5a20ec21c8417520d88a869a93f
nara04040 <nara04040@naver.com>
2022-11-01 18:28:41 +0900

    [JS] 깊은복사를 하는 여러가지 방법 추가

--- JS/데이터타입/얕은복사_깊은복사.js
+++ JS/데이터타입/얕은복사_깊은복사.js
@@ -28,20 +28,45 @@ var copyObject = function (target) {
 // console.log(user.urls.blog === user2.urls.blog); //true
 
 // 중첩된 객체에 대한 깊은 복사
-var user = {
-  name: "Jaenam",
-  urls: {
-    portfolio: "http://github.com/abc",
-    blog: "http//blog.com",
-    facebook: "http://facebook.com/abc",
-  },
+// var user = {
+//   name: "Jaenam",
+//   urls: {
+//     portfolio: "http://github.com/abc",
+//     blog: "http//blog.com",
+//     facebook: "http://facebook.com/abc",
+//   },
+// };
+// var user2 = copyObject(user);
+// user2.urls = copyObject(user.urls);
+
+// user.urls.portfolio = "http://portfolio.com";
+// console.log(user.urls.portfolio === user2.urls.portfolio); // false
+
+// user2.urls.blog = "";
+
+// console.log(user.urls.blog === user2.urls.blog); // false
+
+// 객체의 깊은 복사를 수행하는 범용함수
+var copyObjectDeep = function (target) {
+  var result = {};
+  if (typeof target === "object" && target !== null) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep(target[prop]);
+    }
+  } else {
+    result = target;
+  }
+  return result;
 };
-var user2 = copyObject(user);
-user2.urls = copyObject(user.urls);
 
-user.urls.portfolio = "http://portfolio.com";
-console.log(user.urls.portfolio === user2.urls.portfolio); // false
+var obj = {
+  a: 1,
+  b: { c: null, d: [1, 2] },
+};
+var obj2 = copyObjectDeep(obj);
+obj2.a = 3;
+obj2.b.c = 4;
+obj.b.d[1] = 3;
 
-user2.urls.blog = "";
-
-console.log(user.urls.blog === user2.urls.blog); // false
+console.log(obj);
+console.log(obj2);
