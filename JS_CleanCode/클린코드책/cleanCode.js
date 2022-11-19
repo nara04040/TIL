fb5bc2e8f55c42254bf8fae3fc7d83c86dc86d55
nara04040 <nara04040@naver.com>
2022-11-19 19:59:02 +0900

    [JS] cleanCode

--- JS_CleanCode/클린코드책/cleanCode.js
+++ JS_CleanCode/클린코드책/cleanCode.js
@@ -89,3 +89,53 @@ function paintCar(car) {
 // 기본 매개변수가 short circuiting 트릭이나 조건문 보다 깔끔합니다.
 //  종종 short circuiting 트릭보다 깔끔합니다. 매개변수가 undefined일때만 적용된다.
 // '', "", false, null, 0 , NaN 등 falsy한 값들이 기본 매개변수가 적용되지 않는다.
+
+// Bad Case
+function createMicrobrewery(name) {
+  const breweryName = name || "Hipster Brew Co.";
+}
+// Good Case
+function createMicrobrewery(name = "Hipster Brew Co.") {}
+
+// Functions 함수
+// 함수 인자는 2개 이하가 이상적이다.
+// 매개변수의 개수를 제한 하는 것이 함수 테스팅을 쉽게 만들어 주기 때문에 중요하다.
+// 만약 매개변수가 3개 이상일 경우에는 테스트 해야하는 경우의 수가 많아지고 각기 다른 인수들로 여러 사례들을 테스트 해야한다.
+
+// Bad Case
+function createMenu(title, body, buttonText, cancellable) {
+  // ~~
+}
+
+// Good Case
+function createMenu({ title, body, buttonText, cancellable }) {
+  // ~~
+}
+
+createMenu({
+  title: "Foo",
+  body: "Bar",
+  buttonText: "Baz",
+  cancellable: true,
+});
+
+// 함수는 하나의 행동만 해야한다.
+
+// Bad Case
+function emailClients(clients) {
+  clients.forEach((client) => {
+    const clientRecord = database.lookup(client);
+    if (clientRecord.isActive()) {
+      email(client);
+    }
+  });
+}
+
+// Good Case
+function emailClients(clients) {
+  clients.filter(isClientActive).forEach(email);
+}
+function isClientActive(client) {
+  const clientRecord = database.lookup(client);
+  return clientRecord.isActive();
+}
